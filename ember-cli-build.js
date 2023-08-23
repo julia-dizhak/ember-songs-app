2901bacdfaf2c79121d677e9384cf2b7331e7b9e
Yulia <yulia.scherbina@gmail.com>
2023-08-23 07:46:15 +0200

    add css: tailwind, postcss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [{ module: require('tailwindcss') }],
+      },
+    },
   });
 
   return app.toTree();
